98fa5103f3aab4abeced13a84bffdb52df148993
leomet07 <leomet07@gmail.com>
2025-06-11 13:56:03 -0400

    Mass run r2s for all lakes in a download dir

--- apply_equations.py
+++ apply_equations.py
@@ -7,6 +7,7 @@ import inspect_shapefile
 from shapely.geometry import Point
 from sklearn.metrics import r2_score
 import rasterio.features
+import warnings
 
 
 def apply_equation_to_tif(tif_path):
@@ -23,13 +24,17 @@ def apply_equation_to_tif(tif_path):
         band4 = src.read(4)
         band5 = src.read(5)
 
-        ratio3to5 = band3 / band5
+        with np.errstate(divide="ignore", invalid="ignore"):
+            ratio3to5 = (
+                band3 / band5
+            )  # masked out parts may be nan, dividing nan by nan is invalid
 
-        b0 = 23.5
-        b1 = -36
-        b2 = 0.004
+            b0 = 23.5
+            b1 = -36
+            b2 = 0.004
 
-        y = b0 + b1 * (ratio3to5) + b2 * (band4)
+            y = b0 + b1 * (ratio3to5) + b2 * (band4)
+
         return (
             y,
             profile,
@@ -41,69 +46,93 @@ def apply_equation_to_tif(tif_path):
         )
 
 
-tif_folder = "queer_lake_tifs"
+out_folder = "all_lake_images"
 
-display = True
+display = False
 
-doc_values = []
-predicted_a440_values = []
+for subfolder in os.listdir(out_folder):
+    doc_values = []
+    predicted_a440_values = []
 
-for filename in os.listdir(tif_folder):
-    tif_filepath = os.path.join(tif_folder, filename)
+    tif_folder_path = os.path.join(out_folder, subfolder)
 
-    output_ln_a440, profile, transform, scale, x_res, closest_insitu_date, objectid = (
-        apply_equation_to_tif(tif_filepath)
-    )
+    for filename in os.listdir(tif_folder_path):
+        tif_filepath = os.path.join(tif_folder_path, filename)
 
-    a440 = np.exp(
-        output_ln_a440
-    )  # a440 is absorptivity of filtered water at 440nm wavelength, a measure of CDOM, proportional to DOC
+        (
+            output_ln_a440,
+            profile,
+            transform,
+            scale,
+            x_res,
+            closest_insitu_date,
+            objectid,
+        ) = apply_equation_to_tif(tif_filepath)
 
-    # matched doc
-    all_doc = inspect_shapefile.truth_data[
-        (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
-        & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
-    ]["DOC_MG_L"]
-    doc = all_doc.item()
+        a440 = np.exp(
+            output_ln_a440
+        )  # a440 is absorptivity of filtered water at 440nm wavelength, a measure of CDOM, proportional to DOC
 
-    # get lat and long
-    centroid_lat = inspect_shapefile.truth_data[
-        (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
-        & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
-    ]["Lat-Cent"].item()
-    centroid_long = inspect_shapefile.truth_data[
-        (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
-        & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
-    ]["Lon-Cent"].item()
+        # matched doc
+        all_doc = inspect_shapefile.truth_data[
+            (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
+            & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
+        ]["DOC_MG_L"]
+        try:
+            doc = all_doc.item()
+        except ValueError:  # array either has 2+ or 0 items
+            if len(all_doc) > 0:  # means 2+ measurements for that date, take mean
+                doc = all_doc.mean()
+            else:
+                raise Exception("No DOC values found for that date.")
 
-    radius_in_meters = 60
-    circle = Point(centroid_long, centroid_lat).buffer(
-        x_res * (radius_in_meters / float(scale))
-    )  # however many x_res sized pixels needed for buffer of radius at downloaded scale
+        # get lat and long
+        centroid_lat = inspect_shapefile.truth_data[
+            (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
+            & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
+        ]["Lat-Cent"].iloc[
+            0
+        ]  # take first entry, lake centroid lat will be the same for any matched insitu
+        centroid_long = inspect_shapefile.truth_data[
+            (inspect_shapefile.truth_data["OBJECTID"] == float(objectid))
+            & (inspect_shapefile.truth_data["DATE_SMP"] == closest_insitu_date)
+        ]["Lon-Cent"].iloc[
+            0
+        ]  # take first entry, lake centroid long will be the same for any matched insitu
 
-    outside_circle_mask = rasterio.features.geometry_mask(
-        [circle], a440.shape, transform
-    )
+        radius_in_meters = 60
+        circle = Point(centroid_long, centroid_lat).buffer(
+            x_res * (radius_in_meters / float(scale))
+        )  # however many x_res sized pixels needed for buffer of radius at downloaded scale
 
-    a440[outside_circle_mask] = np.nan
-    # copy over geo data from tif to output, then get circle of output and take average
+        outside_circle_mask = rasterio.features.geometry_mask(
+            [circle], a440.shape, transform
+        )
 
-    if display:
-        title = f"Prediction for Lake-OID-{objectid} on {closest_insitu_date}"
-        fig = plt.figure(title, figsize=(10, 8))
-        plt.imshow(a440, cmap="viridis", interpolation="none")
-        plt.title(title)
-        plt.colorbar()
-        plt.axis("off")
-        plt.show()
+        a440[outside_circle_mask] = np.nan
+        # copy over geo data from tif to output, then get circle of output and take average
 
-    mean_a440 = np.nanmean(a440)
+        if display:
+            title = f"Prediction for Lake-OID-{objectid} on {closest_insitu_date}"
+            fig = plt.figure(title, figsize=(10, 8))
+            plt.imshow(a440, cmap="viridis", interpolation="none")
+            plt.title(title)
+            plt.colorbar()
+            plt.axis("off")
+            plt.show()
 
-    print(doc, mean_a440)
-    if not np.isfinite(mean_a440):  # nanmean can return inf or nan
-        continue
-    doc_values.append(doc)  # true value
-    predicted_a440_values.append(mean_a440)  # predicted value
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            mean_a440 = np.nanmean(a440)
 
-r2 = r2_score(doc_values, predicted_a440_values)
-print("R2 score: ", r2)
+        if not np.isfinite(
+            mean_a440
+        ):  # nanmean can return inf or nan if array is all nans
+            continue
+
+        # only append finite values to r2 comparison
+        doc_values.append(doc)  # true value
+        predicted_a440_values.append(mean_a440)  # predicted value
+
+    r2 = r2_score(doc_values, predicted_a440_values)
+    print(f"R2 score for {subfolder}: ", r2)

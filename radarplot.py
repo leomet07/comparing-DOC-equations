2786318445fcfbeebf3977f60c8a9c0d2bc2bb04
leomet07 <leomet07@gmail.com>
2025-07-22 14:53:35 -0400

    radar plot for applied equation

--- radarplot.py
+++ radarplot.py
@@ -2,36 +2,66 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from math import pi
-
-# Set data
-df = pd.DataFrame(
-    {
-        "lakeid": [
-            298315,
-            298316,
-            298317,
-        ],
-        "eq1_r2": [0, 1, 2],
-        "eq1_rmse": [10, 400, 100],
-        "eq1_mae": [0, 43, 1000],
-    }
-)
+import inspect_shapefile
+import apply_equations
 
 
-# normalize column
-def df_with_normalized_column(df, column):
-    df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())
+def df_with_normalization_across_ten_eqs_by_lake(df, lakeid, categories):
+    # filter df to this lake
+    only_lake_df = df[df["OBJECTID"] == float(lakeid)]
+    only_lake_df_index = only_lake_df.index
+
+    for category in categories:
+        columns_of_interest = list(
+            map(
+                lambda equation_index: f"equation_i{equation_index}_{category}",
+                range(10),
+            )
+        )
+        only_values_of_interest = only_lake_df[columns_of_interest]
+
+        scaled_only_values_of_interest = only_values_of_interest.apply(
+            lambda row: (row - row.min()) / (row.max() - row.min()), axis=1
+        ).values.tolist()[0]
+
+        for i in range(len(scaled_only_values_of_interest)):
+            df.loc[only_lake_df_index, columns_of_interest[i]] = (
+                scaled_only_values_of_interest[i]
+            )
+
     return df
 
 
-df = df_with_normalized_column(df, "eq1_r2")
-df = df_with_normalized_column(df, "eq1_rmse")
-df = df_with_normalized_column(df, "eq1_mae")
-print(df)
+def plot_lake(ax, df, lakeid, equation_index, columns_to_plot, fill_color):
+    lake_row = df[df["OBJECTID"] == float(lakeid)]
+    lake_row = lake_row[columns_to_plot]
 
+    values = lake_row.values.tolist()[0]
+
+    # add first value back so polygon loops around
+    values += values[0:1]
+
+    ax.plot(angles, values, label=f"Equation Index {equation_index}")
+    ax.fill(angles, values, alpha=0.1)
+
+
+# for a specific lake
+alg_name = apply_equations.out_folder.split("_")[
+    -1
+].upper()  # our convention is last underscore contains alg name
+lake_of_interest = inspect_shapefile.lake_infos_of_interest[1]
+lakeid = lake_of_interest["OBJECTID"]
+lakename = lake_of_interest["NAME"]
+
+
+# -------------------------- Radar plot ---------------------------------
 # number of variable
-columns_to_plot = ["eq1_r2", "eq1_rmse", "eq1_mae"]
-N = len(columns_to_plot)
+categories = ["r2", "rmse", "mae"]
+N = len(categories)
+
+df = df_with_normalization_across_ten_eqs_by_lake(
+    apply_equations.results_df, lakeid, categories
+)
 
 # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
 angles = [n / float(N) * 2 * pi for n in range(N)]
@@ -45,7 +75,7 @@ ax.set_theta_offset(pi / 2)
 ax.set_theta_direction(-1)
 
 # Draw one axe per variable + add labels
-plt.xticks(angles[:-1], columns_to_plot)
+plt.xticks(angles[:-1], categories)
 
 # Draw ylabels
 ax.set_rlabel_position(0)
@@ -53,21 +83,14 @@ plt.yticks([0, 0.5, 1], ["0.1", "0.5", "1.0"], color="grey", size=7)
 plt.ylim(0, 1)
 
 
-def plot_lake(ax, df, lakeid, columns_to_plot, fill_color):
-    lake_row = df[df["lakeid"] == lakeid]
-    lake_row = lake_row[columns_to_plot]
-
-    values = lake_row.values.tolist()[0]
-
-    print(values)
-    # add first value back so polygon loops around
-    values += values[0:1]
-
-    # ax.plot(angles, values, linewidth=1, linestyle="solid", label=f"lake{lakeid}")
-    ax.fill(angles, values, fill_color, alpha=0.1)
+# plot every equation
+for equation_index in range(10):
+    columns_to_plot = list(
+        map(lambda category: f"equation_i{equation_index}_{category}", categories)
+    )
+    plot_lake(ax, df, lakeid, equation_index, columns_to_plot, "r")
 
 
-plot_lake(ax, df, 298316, columns_to_plot, "r")
-plot_lake(ax, df, 298317, columns_to_plot, "b")
-
+ax.legend()
+plt.title(f"{alg_name} performance for {lakename}")
 plt.show()
